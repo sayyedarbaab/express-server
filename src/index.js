3b6e8c5a374d2c64b5342b4075b67d1dd4e529ef
Sayyed Arbaab <kazmiwrite@gmail.com>
2024-03-01 19:53:14 +0530

    add commit

---
+++ src/index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/images", express.static(path.join(__dirname, "../public/images")));
+
+app.get("/", (req, res) => {
+    console.log(path.join(__dirname, "../public"));
+    const foodData = [
+        {
+            name: "Boiled Egg",
+            price: 10,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/egg.png",
+            type: "breakfast",
+        },
+        {
+            name: "RAMEN",
+            price: 25,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/ramen.png",
+            type: "lunch",
+        },
+        {
+            name: "GRILLED CHICKEN",
+            price: 45,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/chicken.png",
+            type: "dinner",
+        },
+        {
+            name: "CAKE",
+            price: 18,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/cake.png",
+            type: "breakfast",
+        },
+        {
+            name: "BURGER",
+            price: 23,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/burger.png",
+            type: "lunch",
+        },
+        {
+            name: "PANCAKE",
+            price: 15,
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+            image: "/images/pancake.png",
+            type: "dinner",
+        },
+    ];
+    res.json(foodData);
+});
+
+const PORT = process.env.PORT || 9200;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
